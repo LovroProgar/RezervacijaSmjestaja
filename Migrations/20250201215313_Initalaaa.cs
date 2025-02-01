4a2cd0bf3852496ff021d964faae359ea15356a5
Lovro Progar <loprogar@vsite.hr>
2025-02-02 00:05:47 +0100

    radi radi

---
+++ Migrations/20250201215313_Initalaaa.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RezervacijaSmjestaja.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initalaaa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
