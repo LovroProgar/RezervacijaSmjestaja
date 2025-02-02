1de81a591989694488e87477dfd9f340e65dffda
Lovro Progar <loprogar@vsite.hr>
2025-02-02 12:16:28 +0100

    nesto

---
+++ Migrations/20250201235313_1adaaaa.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RezervacijaSmjestaja.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1adaaaa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rezervacije_Korisnici_KorisnikId",
+                table: "Rezervacije");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rezervacije_KorisnikId",
+                table: "Rezervacije");
+
+            migrationBuilder.DropColumn(
+                name: "KorisnikId",
+                table: "Rezervacije");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KorisnikId",
+                table: "Rezervacije",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rezervacije_KorisnikId",
+                table: "Rezervacije",
+                column: "KorisnikId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rezervacije_Korisnici_KorisnikId",
+                table: "Rezervacije",
+                column: "KorisnikId",
+                principalTable: "Korisnici",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Migrations/20250202111252_NazivMigracije.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RezervacijaSmjestaja.Migrations
+{
+    /// <inheritdoc />
+    public partial class NazivMigracije : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
