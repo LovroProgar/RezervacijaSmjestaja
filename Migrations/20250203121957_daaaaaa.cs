735bf623ea54de3593e51b89fcf45e27d3233a04
Lovro Progar <loprogar@vsite.hr>
2025-02-03 13:26:14 +0100

    zavrsni commit

---
+++ Migrations/20250203121957_daaaaaa.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RezervacijaSmjestaja.Migrations
+{
+    /// <inheritdoc />
+    public partial class daaaaaa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Smjestaji",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "SlikaUrl",
+                value: "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?w=1280");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Smjestaji",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "SlikaUrl",
+                value: "https://images.pexels.com/photos/1643386/pexels-photo-1643386.jpeg?w=1280");
+        }
+    }
+}
